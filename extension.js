e9ffac6f8083fc8f8e0656b03cb980f6f6c73c3f
Allar <allar@michaelallar.com>
2017-05-16 15:13:17 -0700

    Better.

--- extension.js
+++ extension.js
@@ -4,14 +4,7 @@ var request = require('request');
 
 module.exports = function (nodecg) 
 {
-
-    var lastfmapi = {
-        api_key: "7e733d81e3f9599e79541cbc96ae5b6a",
-        secret_key: "02842f6d3de4efac9b7e63bf1fdbdf70",
-        user: "awesomeallar"
-    };
-
-    var lastfm_uri = `http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${lastfmapi.user}&api_key=${lastfmapi.api_key}&limit=1&format=json`
+    var lastfm_uri = `http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${nodecg.bundleConfig.lastfm_user}&api_key=${nodecg.bundleConfig.lastfm_api_key}&limit=1&format=json`
 
     var ArtistName = nodecg.Replicant('ArtistName', {defaultValue: 'Unknown'});
     var TrackName = nodecg.Replicant('TrackName', {defaultValue: 'Unknown'});
@@ -19,7 +12,6 @@ module.exports = function (nodecg)
 
     var updateInfo = function() 
     {
-
         request(lastfm_uri, function(err, res, body) 
         {
             if (!err && res.statusCode == 200) 
@@ -66,5 +58,4 @@ module.exports = function (nodecg)
     }
 
     setInterval(updateInfo, 5000);
-
 };
